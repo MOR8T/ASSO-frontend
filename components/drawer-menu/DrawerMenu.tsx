3c927c5060b72d739f711694a089859a64222236
Shukurov Shahrom <hp.sh.shahrom@gmail.com>
2026-02-14 23:42:01 +0500

    Make DrawerMenu component

---
+++ components/drawer-menu/DrawerMenu.tsx
@@ -0,0 +1,103 @@
+"use client";
+import { useEffect } from "react";
+import Link from "next/link";
+import home_icon from "@/public/images/icons/home.svg";
+import Image from "next/image";
+
+export interface DrawerItem {
+  id: string;
+  title: string;
+  url?: string;
+  children?: {
+    id: string;
+    title: string;
+    url: string;
+  }[];
+}
+
+interface DrawerProps {
+  isOpen: boolean;
+  onClose: () => void;
+  menu: DrawerItem[];
+}
+
+export const DrawerMenu = ({ isOpen, onClose, menu }: DrawerProps) => {
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+
+    if (isOpen) {
+      document.addEventListener("keydown", handleEsc);
+      document.body.style.overflow = "hidden";
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleEsc);
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen, onClose]);
+
+  return (
+    <>
+      {/* Overlay */}
+      <div
+        className={`fixed inset-0 z-40 transition-opacity duration-300 ${
+          isOpen ? " opacity-100" : "pointer-events-none opacity-0"
+        }`}
+        onClick={onClose}
+      />
+      {/* Drawer */}
+      <div
+        className={`fixed top-0 right-0 z-50 h-full w-full sm:w-[320px] sm:mt-[101px]
+        bg-[rgba(43,53,59,0.85)]
+        shadow-2xl transform transition-transform duration-300 ease-in-out
+        ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+      >
+        <div className="h-full flex flex-col justify-between p-[50px_20px_180px_50px] overflow-y-auto">
+          <div className="space-y-7">
+            {menu?.map((section, s_i) => (
+              <div
+                key={section.id}
+                className={`text-[#DBDBDB] font-stretch-condensed`}
+              >
+                <Link
+                  href={`/${section?.url ?? "#"}`}
+                  className={`block text-orange-500 uppercase font-extralight text-[22px] mb-4 hover:text-orange-400 transition 
+                  ${menu?.length == s_i + 1 && section?.children?.length == 0 ? "" : "border-b border-[#5B626B] pb-1"}`}
+                  onClick={onClose}
+                >
+                  {section.title}
+                </Link>
+                {section?.children && (
+                  <div className="space-y-3">
+                    {section?.children?.map((child, i) => (
+                      <Link
+                        key={child.id}
+                        href={`/${child.url}`}
+                        className={`block 
+                        ${menu?.length == s_i + 1 && section?.children?.length == i + 1 ? "" : "border-b border-[#5B626B]"} 
+                        ${section.children?.length == i + 1 ? "pb-6" : "pb-1"} 
+                        text-[22px] font-extralight uppercase hover:text-white transition`}
+                        onClick={onClose}
+                      >
+                        {child.title}
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+
+          {/* Home button */}
+          <div className="flex justify-center mt-10">
+            <Link href="/" onClick={onClose}>
+              <Image src={home_icon} alt="close" width={28} height={28} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,15 @@
+"use client";
 import Image from "next/image";
 import logo from "../../public/images/logos/logo.svg";
 import menuIcon from "@/public/images/icons/menu.svg";
 import LangSwicher from "../lang-swicher/LangSwicher";
 import { Link } from "@/i18n/navigation";
+import { DrawerMenu, DrawerItem } from "@/components/drawer-menu/DrawerMenu";
+import { useState } from "react";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   const headers = [
     {
       id: 1,
@@ -33,6 +38,34 @@ export default function Header() {
     },
   ];
 
+  const menuData: DrawerItem[] = [
+    {
+      id: "1",
+      title: "АРХИТЕКТУРНЫЕ ПРОЕКТЫ",
+      url: "archetectur_project",
+      children: [
+        { id: "1-1", title: "ЧАСТНЫЕ ДОМА", url: "design" },
+        { id: "1-2", title: "ОБЩЕСТВЕННЫЕ ЗДАНИЯ", url: "public_buildings" },
+      ],
+    },
+    {
+      id: "2",
+      title: "ДИЗАЙН ИНТЕРЬЕРЫ",
+      url: "interior_design",
+      children: [
+        { id: "2-1", title: "ДОМА И КОТТЕДЖИ", url: "houses_and_cottages" },
+        { id: "2-2", title: "КВАРТИРЫ", url: "apartments" },
+        { id: "2-3", title: "ОБЩЕСТВЕННЫЕ ИНТЕРЬЕРЫ", url: "public_interiors" },
+      ],
+    },
+    {
+      id: "3",
+      title: "Мебель и освешение",
+      url: "furniture_and_decoration",
+      children: [],
+    },
+  ];
+
   return (
     <header className="w-full px-5 mx-auto">
       <div className="max-w-7xl mx-auto py-7 flex justify-between items-center">
@@ -40,7 +73,7 @@ export default function Header() {
           <Image src={logo} alt="ASSO logo" width={200} height={100} />
         </Link>
         <div className="flex items-center gap-5">
-          <div className="flex items-center gap-3 text-[rgb(219,219,219)] uppercase">
+          <div className="flex items-center gap-5 text-[rgb(219,219,219)] uppercase">
             {headers.map((header) => (
               <Link key={header.id} href={`/${header.value}`}>
                 {header.title}
@@ -48,11 +81,16 @@ export default function Header() {
             ))}
           </div>
           <LangSwicher />
-          <button className="cursor-pointer">
+          <button onClick={() => setIsOpen(true)} className="cursor-pointer">
             <Image src={menuIcon} alt="Menu icon" width={28} height={28} />
           </button>
         </div>
       </div>
+      <DrawerMenu
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        menu={menuData}
+      />
     </header>
   );
 }
